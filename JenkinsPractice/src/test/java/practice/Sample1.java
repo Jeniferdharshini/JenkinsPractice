712a14e6ea7545c98baeabd048dd8e3fc04dc5a9
Jenifer <103610709+Jeniferdharshini@users.noreply.github.com>
2023-03-30 17:44:22 -0500

    Update Sample1.java

--- JenkinsPractice/src/test/java/practice/Sample1.java
+++ JenkinsPractice/src/test/java/practice/Sample1.java
@@ -4,6 +4,14 @@ public class Sample1 {
 
 	public static void main(String[] args) {
 		System.out.println("main method running");
+		
+		//jenkins run periodically=>  Minutes hours date month week
+		// minutes(0-59)
+		//hour(0-23)
+		// day(1-31)
+		//month(1-12)
+		//day of week(0-7)
+		
 		//pull back
 		//push to eclipse
 		//push to git
